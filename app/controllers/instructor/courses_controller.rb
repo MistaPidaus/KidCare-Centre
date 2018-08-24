b38bcd4e46f70ee60539eaaf792df841a519b41f
MistaPidaus <firdaus.kamal581@gmail.com>
2018-08-24 15:03:00 +0800

    Create cert after course

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -31,9 +31,10 @@ class Instructor::CoursesController < ApplicationController
     @course = Course.new(course_params)
     @course.creator_id = current_user.id if current_user
     authorize! :create, @course
+    @certificate = Certificate.new(:name => @course.name, :description => @course.description, :file, :course_id => @course.id)
 
     respond_to do |format|
-      if @course.save
+      if @course.save && @certificate.save
         format.html { redirect_to @course, notice: 'Course was successfully created.' }
         format.json { render :show, status: :created, location: @course }
       else
@@ -45,8 +46,10 @@ class Instructor::CoursesController < ApplicationController
 
   def update
     authorize! :update, @course
+    @certificate = Certificate.find(params[:id])
+    cert_params = (:name => @course.name, :description => @course.description, :file, :course_id => @course.id)
     respond_to do |format|
-      if @course.update(course_params)
+      if @course.update(course_params) && @certificate.update(cert_params)
         format.html { redirect_to @course, notice: 'Course was successfully updated.' }
         format.json { render :show, status: :ok, location: @course }
       else
